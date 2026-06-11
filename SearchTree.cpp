6b8c6e5c0476cb9aeccc1388a319f3ef8c9f021f
sabrinakayla <sabrinakuliah10@gmail.com>
2026-06-11 11:33:34 +0700

    implementasi penelusuran left-right

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -117,4 +117,12 @@ public:
             cout << "Tree is empty" << endl;
             return;
         }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
