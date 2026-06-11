526d6ccb0db50cbab7286a023b6fd91c17504133
sabrinakayla <sabrinakuliah10@gmail.com>
2026-06-11 11:37:15 +0700

    inisialisasi awal fungsi main

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -97,7 +97,7 @@ public:
     {
         if (ROOT == NULL)
         {
-            cout <<"Tree is empty"
+            cout <<"Tree is empty" << endl;
         }
 
         if (ptr != NULL)
@@ -126,3 +126,22 @@ public:
         }
     }
 };
+
+int main()
+{
+    BinaryTree obj;
+
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
