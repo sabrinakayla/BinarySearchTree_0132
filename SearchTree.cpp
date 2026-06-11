5a2e667fd0e87b8402c136027a0d150200350a3e
sabrinakayla <sabrinakuliah10@gmail.com>
2026-06-11 11:20:59 +0700

    lohika validasi pohon kosong pada inorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -76,3 +76,11 @@ public:
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
