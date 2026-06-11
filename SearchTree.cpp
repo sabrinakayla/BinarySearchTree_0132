eed957a7c82428ccb6ab31f443878c795a84894a
sabrinakayla <sabrinakuliah10@gmail.com>
2026-06-11 11:29:33 +0700

    implementasi urutan cetak penelusuran

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -92,11 +92,18 @@ public:
             inorder(ptr->rightchild);
         }
 
-        void preorder(Node *ptr)
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
         {
-            if (ROOT == NULL)
-            {
-                cout <<"Tree is empty"
-            }
+            cout <<"Tree is empty"
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
         }
     }
+}
